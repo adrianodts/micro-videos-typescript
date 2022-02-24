cf071d4f9b34ac5472bb31cdae04d8108f3a32b3
Adriano Dantas <adrianodts@msn.com>
2022-02-24 12:00:43 -0300

    included eslit and test refactoring

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,49 +1,49 @@
+/* eslint-disable class-methods-use-this */
 export interface CategoryProperties {
-    name: string; 
-    description?: string; 
+    name: string;
+    description?: string;
     isActive?: boolean;
     createdAt?: Date;
 }
 
 export class Category implements CategoryProperties {
-
-    constructor(public readonly properties: CategoryProperties) {
-        if (!this.properties) {
-            console.log(properties);
-            throw new Error("Category is null or undefined!");
-        }
-        this.name = this.properties.name;
-        this.description = this.properties.description;
-        this.isActive = this.properties.isActive;
-        this.properties.createdAt = properties.createdAt ?? new Date();
+  constructor(public readonly properties: CategoryProperties) {
+    if (!this.properties) {
+      throw new Error('Category is null or undefined!');
     }
+    this.name = this.properties.name;
+    this.description = this.properties.description;
+    this.isActive = this.properties.isActive;
+    this.properties.createdAt = properties.createdAt ?? new Date();
+  }
 
-    get name(): string {
-        return this.properties.name;
-    }
+  get name(): string {
+    return this.properties.name;
+  }
 
-    private set name(value: string) {
-        if (value === null || value === undefined || value.trim().length === 0) 
-            throw new Error("Category name is required!")
+  private set name(value: string) {
+    if (value === null || value === undefined || value.trim().length === 0) {
+      throw new Error('Category name is required!');
     }
+  }
 
-    get description(): string | undefined {
-        return this.properties.description;
-    }
+  get description(): string | undefined {
+    return this.properties.description;
+  }
 
-    private set description(value: string) {
-        this.properties.description = value ?? null;
-    }
+  private set description(value: string) {
+    this.properties.description = value ?? null;
+  }
 
-    get isActive(): boolean | undefined {
-        return this.properties.isActive;
-    }
+  get isActive(): boolean | undefined {
+    return this.properties.isActive;
+  }
 
-    private set isActive(value: boolean) {
-        this.properties.isActive = value ?? true;
-    }
+  private set isActive(value: boolean) {
+    this.properties.isActive = value ?? true;
+  }
 
-    get createdAt(): Date | undefined {
-        return this.properties.createdAt;
-    }
+  get createdAt(): Date | undefined {
+    return this.properties.createdAt;
+  }
 }
